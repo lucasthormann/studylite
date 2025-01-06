924fc5f55b228f99c2e9f6ae144ab538ca204187
lucasthormann <LucasThormann@protonmail.com>
2025-01-06 13:14:15 -0600

    bug fix (edit/delete rooms)

--- base/views.py
+++ base/views.py
@@ -110,7 +110,6 @@ def createRoom(request):
     if request.method == 'POST':
         topic_name = request.POST.get('topic')
         topic, created = Topic.objects.get_or_create(name=topic_name)
-        form = RoomForm(request.POST)
 
         Room.objects.create(
             host=request.user,
@@ -133,12 +132,15 @@ def updateRoom(request, pk):
         return HttpResponse('You are not allowed here!!')
 
     if request.method == 'POST':
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
 
-    context = {'form': form, 'topics' : topics}
+    context = {'form': form, 'topics' : topics, 'room': room}
     return render(request, 'base/room_form.html', context)
 
 @login_required(login_url='/login')
